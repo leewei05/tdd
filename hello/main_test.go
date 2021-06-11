c87bcf9221a9509171257d953cba49035864bab0
Lee Wei <leewei@17.media>
2021-06-12 06:28:02 +0800

    refactor test code with helper function

--- hello/main_test.go
+++ hello/main_test.go
@@ -4,21 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("Say hello to people", func(*testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// Telling test suite that this method is a helper
+		// By defining Helper function, it will report the line number we fail than
+		// inside our helper function
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("Say hello to people", func(*testing.T) {
+		got := Hello("Chris")
+		want := "Hello, hris"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is applied", func(*testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
