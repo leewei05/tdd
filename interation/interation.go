ad721eb6f667a1aa41190a9493f4cf68c96df9d0
Lee Wei <leewei@17.media>
2021-06-13 09:11:45 +0800

    refactor with const repeatCount

--- interation/interation.go
+++ interation/interation.go
@@ -1,8 +1,10 @@
 package interation
 
+const repeatedCount = 5
+
 func Repeat(character string) string {
 	res := ""
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatedCount; i++ {
 		res += character
 
 	}
