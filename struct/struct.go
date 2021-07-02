d4bccbe71ad497ba4af455addff9098118527d92
Lee <lee10202013@gmail.com>
2021-07-02 12:52:08 +0800

    create Area function

--- struct/struct.go
+++ struct/struct.go
@@ -3,3 +3,7 @@ package shapes
 func Perimeter(width, height float64) (perimeter float64) {
 	return 2 * (width + height)
 }
+
+func Area(width, height float64) (area float64) {
+	return width * height
+}
