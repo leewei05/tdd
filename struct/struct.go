e48b6079b5394220019ecaad855789a1343266d2
Lee <lee10202013@gmail.com>
2021-07-02 13:36:59 +0800

    refactor area methods with different struct type

--- struct/struct.go
+++ struct/struct.go
@@ -1,14 +1,23 @@
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) (perimeter float64) {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) (area float64) {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() (area float64) {
+	return r.Width * r.Height
+}
+func (c Circle) Area() (area float64) {
+	return c.Radius * c.Radius * math.Pi
 }
