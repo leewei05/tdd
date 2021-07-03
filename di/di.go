6315554d23ebe3da266a7983f23354cd640b360b
Lee <lee10202013@gmail.com>
2021-07-03 17:12:01 +0800

    add comment for io.Writer

--- di/di.go
+++ di/di.go
@@ -8,3 +8,8 @@ import (
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
+
+/*
+By using io.Writer, we can pass io.Stdout, bytes.Buffer type into Greet.
+Because those types implement io.Writer with Write(p []byte) (n int, err error)
+*/
