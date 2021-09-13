56c338b028c9d4b3bad70b199ee0e60988476148
Lee <lee10202013@gmail.com>
2021-09-13 23:34:46 +0800

    refactor counter test

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -9,8 +9,13 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter.count, 3)
 	})
+}
+
+func assertCounter(t testing.TB, got int, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
