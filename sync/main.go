4365e949668ccbcd3d89380283c0ad41687c3b30
Lee <lee10202013@gmail.com>
2021-09-13 23:48:33 +0800

    add mutex to Counter

--- sync/main.go
+++ sync/main.go
@@ -1,11 +1,16 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	count int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
 	c.count++
+	c.mu.Unlock()
 }
 
 func (c *Counter) Value() int {
