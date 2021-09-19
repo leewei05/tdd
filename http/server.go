6ee52cd0280c440542ab154239cdb2fdc2b94f4a
Lee <lee10202013@gmail.com>
2021-09-19 16:00:43 +0800

    refactor for getting score of two players

--- http/server.go
+++ http/server.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(player string) string {
+	switch player {
+	case "lee":
+		return "20"
+	case "greg":
+		return "10"
+	}
+
+	return ""
 }
