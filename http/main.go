0492f3da9669daa2f8096cf367b200ada28142d2
Lee <lee10202013@gmail.com>
2021-09-21 21:26:21 +0800

    PlayerStore for storing player scores

--- http/main.go
+++ http/main.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct{}
+
 func main() {
-	server := &PlayerServer{}
+	playerStore := InMemoryPlayerStore{}
+	server := &PlayerServer{&playerStore}
 	log.Fatal(http.ListenAndServe(":8000", server))
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+
+	return 123
 }
