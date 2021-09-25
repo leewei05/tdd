dd98f3075071e5cd1ff3d361bdb85e1ea707f31e
Lee <lee10202013@gmail.com>
2021-09-25 21:01:50 +0800

    replace InMemoryStore with FileSystemPlayerStore

--- http/server_integration_test.go
+++ http/server_integration_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
-	store := NewInMemoryPlayerStore()
+	database, cleanDB := createTempFile(t, "")
+	defer cleanDB()
+	store := &FileSystemPlayerStore{database}
 	server := NewPlayerServer(store)
 	player := "Pepper"
 
