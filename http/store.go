8be3da90df97bae82a0974868064ad0884d4fd71
Lee <lee10202013@gmail.com>
2021-09-26 14:57:30 +0800

    initialize db file

--- http/store.go
+++ http/store.go
@@ -11,16 +11,25 @@ type FileSystemPlayerStore struct {
 	league League
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initialilzePlayerDBFile(file *os.File) error {
 	file.Seek(0, 0)
 	info, err := file.Stat()
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	if info.Size() == 0 {
 		file.Write([]byte("[]"))
 		file.Seek(0, 0)
+	}
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initialilzePlayerDBFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
