ddd806f63cbda6dbf249257b28f47cd9b96a2919
Lee <lee10202013@gmail.com>
2021-09-26 14:38:54 +0800

    refactor with *os.File type

---
+++ http/tape_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestTapeWrite(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	tape := &tape{file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, 0)
+	newFileContent, _ := ioutil.ReadAll(file)
+
+	got := string(newFileContent)
+	want := "abc"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
