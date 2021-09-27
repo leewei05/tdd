48f48ad7c953d8dcd46e71281f0c4523be8be442
Lee <lee10202013@gmail.com>
2021-09-27 11:09:25 +0800

    refactor test code with assertPlayerWin

--- http/cli_test.go
+++ http/cli_test.go
@@ -11,13 +11,5 @@ func TestCLI(t *testing.T) {
 	cli := &CLI{store, in}
 	cli.PlayPoker()
 
-	if len(store.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
-	}
-
-	got := store.winCalls[0]
-	want := "Lee"
-	if got != want {
-		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
-	}
+	assertPlayerWin(t, store, "Lee")
 }
--- http/server_test.go
+++ http/server_test.go
@@ -94,10 +94,10 @@ func TestGETPlayers(t *testing.T) {
 }
 
 func TestStoreWins(t *testing.T) {
-	store := StubPlayerStore{
+	store := &StubPlayerStore{
 		scores: map[string]int{},
 	}
-	server := NewPlayerServer(&store)
+	server := NewPlayerServer(store)
 	player := "lee"
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
@@ -107,14 +107,7 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
-
-		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
+		assertPlayerWin(t, store, player)
 	})
 }
 
@@ -170,5 +163,17 @@ func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want s
 	t.Helper()
 	if response.Result().Header.Get("content-type") != want {
 		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
+}
+
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
 	}
 }
